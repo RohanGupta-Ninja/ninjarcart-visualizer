c4556aa5febc406c3a8961fb5dcd51fa28f2c085
Rohan Gupta <rohang@ninjacart.com>
2023-02-01 22:57:01 +0530

    [dev]:parsed api data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import datas from './data.json';
+import {data} from './data';
 
 function App() {
+const res=data.apiComponentList;
+const apiData=res.map((item,index)=>{
+  return item.apiConfigDefinition.apiConfigReference
+})
   return(
-    <div>
-      {
-        datas.map(data => {
-          return(
-            <div className = "box">
-              {data.apiComponentList}
-              <h1>Rohan</h1>
-            </div>
-
-          )
-        })
-      }
+    <div >
+      {apiData.length>0 && apiData.map((item,index)=>{
+        return (<div className="api-node-container"  key={index}>
+          <div className="node-heading">API {index+1}</div>
+          <div>ServiceName: {item.serviceName}</div>
+          <div>Verb: {item.verb}</div>
+          <div>URL: {item.url}</div>
+          </div>)
+      })}
     </div>
   )
 }
